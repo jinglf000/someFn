e19481561c98f651ca73bb78f6b006515c43e180
jinglf000 <jinglongfei@jsb5.com>
2018-07-06 18:18:16 +0800

    添加了结构的内容

---
+++ src/dataStructure/structure.js
@@ -0,0 +1,70 @@
+const set = new Set();
+const map = new Map();
+const obj = { name: 'My name is Object', age: 12 };
+const arr = [12,4,5,8,0];
+const symbol = Symbol('hello');
+
+set.add('12');
+set.add(12);
+set.add(undefined);
+set.add(undefined);
+set.add(null);
+set.add(map);
+set.add(obj);
+set.add(symbol);
+
+// console.log(Symbol('hello') === Symbol('hello'));
+// console.log(set.keys());
+// console.log(set.values());
+// console.log(set);
+
+// for (let i of set.entries()) console.log(i);
+// console.log(arr.keys());
+// for (let i of arr.entries()) console.log(i); iterator
+
+obj[symbol] = 'symbol base';
+
+map.set(arr, 'this is an arr');
+
+map.set(map, 'this is a map');
+
+map.set(map, map);
+
+map.helloworld = 'helloworld';
+
+map[map] = map;
+
+// console.log(map.toString());
+toString = Object.prototype.toString
+
+// console.log(toString.call(set));// [object Set]
+// console.log(toString.call(map));// [object Map]
+
+// console.log(map['[object Map]']) ==> map
+// console.log(map[map]) map会执行toString的操作
+// 
+
+// for (let i in map) { console.log(i) }// map虽然是Map数据类型，同时也是对象，可以使用for in 对对象进行循环遍历
+// for (let i of map.values()) { console.log(i)} // for of 主要针对于遍历器：values() keys() entries() 
+// 当使用for...of循环遍历某种数据结构时，该循环会自动去寻找 Iterator 接口。会不断的调用Iterator接口的next方法获取值，
+// 进而实现了遍历
+
+var it = makeIterator(['a', 'b']);
+
+function makeIterator(arr) {
+	var index = 0;
+
+	return {
+		[Symbol.iterator]() {
+			return {
+				next() {
+					return index ++ < arr.length ? 
+					{ value: arr[index], done: false } :
+					{ value: undefined, done: true }
+				}
+			}
+		}
+	}
+}
+
+for (let i of it()) console.log(i);
