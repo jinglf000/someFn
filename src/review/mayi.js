080b29338576b08d57af3848a8e1db78a7daa304
jinglf000 <jinglf000@gmail.com>
2018-09-28 18:55:05 +0800

    review content

---
+++ src/review/mayi.js
@@ -0,0 +1,22 @@
+// 1、判断字符串回？（）
+
+// var a = '1234321';
+
+// function getSame(obj) {
+//   var arr = [];
+//   var arrNew = [];
+//   if (obj.length % 2 == 0) {
+//     var j = obj.length;
+//     for (var i = 0; i < obj.length; i++) {
+//       arr.push(obj[i]);
+//       console.log(obj[j - (i + 1)]);
+//       if (obj[i] == obj[j - (i + 1)]) {
+//         console.log(111);
+//         return false;
+//       }
+//     }
+//   } else {
+//     console.log('不符合');
+//   }
+// }
+// getSame(a);
