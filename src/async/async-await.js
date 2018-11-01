fe5b1db51690de161d403eed8fde6c950fc715d9
scar.jing <scar.jing@qunar.com>
2018-11-01 11:08:29 +0800

    async await

---
+++ src/async/async-await.js
@@ -0,0 +1,16 @@
+/**
+ * async await from
+ * https://github.com/frontarm/async-javascript-cheatsheet
+ * @author jinglf000
+ */
+
+Promise.all(); // 并发所有都完成后，才完成
+
+Promise.race();// 以最先完成的状态为准
+
+Promise.finally();// fulfilled or reject
+
+Promise.reject();// return rejected promise
+
+Promise.resolve();// reutrn fulfilled promise
+
